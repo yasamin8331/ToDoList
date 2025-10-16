0caf42caecb74fbffe023934d5976ae0bbb3c68c
yasamin <yasi.tahery83318331@gmail.com>
2025-10-16 02:45:04 -0700

    feat: add NotFoundError and DuplicateError

--- todolist/core/exception.py
+++ todolist/core/exception.py
@@ -12,3 +12,10 @@ class LimitExceededError(ToDoListError):
     """Raised when maximum limits are exceeded."""
     pass
 
+class NotFoundError(ToDoListError):
+    """Raised when a project or task is not found."""
+    pass
+
+class DuplicateError(ToDoListError):
+    """Raised when duplicate items are detected."""
+    pass
